a9d4f6ddb697b3df4f1e34636cf661549352c7da
Alfonso Pacheco <1217849+alcheco@users.noreply.github.com>
2023-10-14 21:47:28 -0300

    ADD: v 0.1.15 multiple fields to quote

--- lib/mobysuite/gc2/quote.rb
+++ lib/mobysuite/gc2/quote.rb
@@ -8,7 +8,7 @@ module Mobysuite
 
       def create payload
         payload = {
-            "rut": payload[:rut],
+            "rut": (payload[:rut].blank? ? payload[:dni] : payload[:rut]),
             "fName": payload[:fName],
             "lName": payload[:lName],
             "email": payload[:email],
@@ -18,7 +18,24 @@ module Mobysuite
             "project": payload[:project_id],
             "assets": payload[:assets],
         }
-        payload["discountId"] = payload[:discountId].to_i unless payload[:discountId].blank?
+        #Validate optional paramss
+        payload.merge!("dni": true) unless payload[:dni].blank?
+        payload.merge!("profesion": payload[:profesion]) unless payload[:profesion].blank?
+        payload.merge!("comuna": payload[:comuna]) unless payload[:comuna].blank?
+        payload.merge!("ageRange": payload[:ageRange]) unless payload[:ageRange].blank?
+        payload.merge!("destinationPurchase": payload[:destinationPurchase]) unless payload[:destinationPurchase].blank?
+        payload.merge!("receiveNews": payload[:receiveNews]) unless payload[:receiveNews].blank?
+        payload.merge!("observation": payload[:observation]) unless payload[:observation].blank?
+        payload.merge!("rentRange": payload[:rentRange]) unless payload[:rentRange].blank?
+        payload.merge!("sex": payload[:sex]) unless payload[:sex].blank?
+        payload.merge!("discountId": payload[:discountId]) unless payload[:discountId].blank?
+        payload.merge!("source": payload[:source]) unless payload[:source].blank?
+        payload.merge!("contact": payload[:contact]) unless payload[:contact].blank?
+        payload.merge!("utm_campaign": payload[:utm_campaign]) unless payload[:utm_campaign].blank?
+        payload.merge!("utm_content": payload[:utm_content]) unless payload[:utm_content].blank?
+        payload.merge!("utm_medium": payload[:utm_medium]) unless payload[:utm_medium].blank?
+        payload.merge!("utm_source": payload[:utm_source]) unless payload[:utm_source].blank?
+        payload.merge!("utm_term": payload[:utm_term]) unless payload[:utm_term].blank?
         set_sender("POST", "integrations/quotes", payload)
       end
     end
