c8822c07d6f211bc5b3ec1d40d207e9589f93eda
Alfonso Pacheco <1217849+alcheco@users.noreply.github.com>
2023-10-14 22:54:30 -0300

    FIX: quote parameters name

--- lib/mobysuite/gc2/quote.rb
+++ lib/mobysuite/gc2/quote.rb
@@ -7,8 +7,8 @@ module Mobysuite
       end
 
       def create payload
-        payload = {
-            "rut": (payload[:rut].blank? ? payload[:dni] : payload[:rut]),
+        data = {
+            "rut": (payload[:rut].nil? ? payload[:dni] : payload[:rut]),
             "fName": payload[:fName],
             "lName": payload[:lName],
             "email": payload[:email],
@@ -19,24 +19,24 @@ module Mobysuite
             "assets": payload[:assets],
         }
         #Validate optional paramss
-        payload.merge!("dni": true) unless payload[:dni].blank?
-        payload.merge!("profesion": payload[:profesion]) unless payload[:profesion].blank?
-        payload.merge!("comuna": payload[:comuna]) unless payload[:comuna].blank?
-        payload.merge!("ageRange": payload[:ageRange]) unless payload[:ageRange].blank?
-        payload.merge!("destinationPurchase": payload[:destinationPurchase]) unless payload[:destinationPurchase].blank?
-        payload.merge!("receiveNews": payload[:receiveNews]) unless payload[:receiveNews].blank?
-        payload.merge!("observation": payload[:observation]) unless payload[:observation].blank?
-        payload.merge!("rentRange": payload[:rentRange]) unless payload[:rentRange].blank?
-        payload.merge!("sex": payload[:sex]) unless payload[:sex].blank?
-        payload.merge!("discountId": payload[:discountId]) unless payload[:discountId].blank?
-        payload.merge!("source": payload[:source]) unless payload[:source].blank?
-        payload.merge!("contact": payload[:contact]) unless payload[:contact].blank?
-        payload.merge!("utm_campaign": payload[:utm_campaign]) unless payload[:utm_campaign].blank?
-        payload.merge!("utm_content": payload[:utm_content]) unless payload[:utm_content].blank?
-        payload.merge!("utm_medium": payload[:utm_medium]) unless payload[:utm_medium].blank?
-        payload.merge!("utm_source": payload[:utm_source]) unless payload[:utm_source].blank?
-        payload.merge!("utm_term": payload[:utm_term]) unless payload[:utm_term].blank?
-        set_sender("POST", "integrations/quotes", payload)
+        data.merge!("dni": true) unless payload[:dni].nil?
+        data.merge!("profesion": payload[:profesion]) unless payload[:profesion].nil?
+        data.merge!("comuna": payload[:comuna]) unless payload[:comuna].nil?
+        data.merge!("ageRange": payload[:ageRange]) unless payload[:ageRange].nil?
+        data.merge!("destinationPurchase": payload[:destinationPurchase]) unless payload[:destinationPurchase].nil?
+        data.merge!("receiveNews": payload[:receiveNews]) unless payload[:receiveNews].nil?
+        data.merge!("observation": payload[:observation]) unless payload[:observation].nil?
+        data.merge!("rentRange": payload[:rentRange]) unless payload[:rentRange].nil?
+        data.merge!("sex": payload[:sex]) unless payload[:sex].nil?
+        data.merge!("discountId": payload[:discountId]) unless payload[:discountId].nil?
+        data.merge!("source": payload[:source]) unless payload[:source].nil?
+        data.merge!("contact": payload[:contact]) unless payload[:contact].nil?
+        data.merge!("utm_campaign": payload[:utm_campaign]) unless payload[:utm_campaign].nil?
+        data.merge!("utm_content": payload[:utm_content]) unless payload[:utm_content].nil?
+        data.merge!("utm_medium": payload[:utm_medium]) unless payload[:utm_medium].nil?
+        data.merge!("utm_source": payload[:utm_source]) unless payload[:utm_source].nil?
+        data.merge!("utm_term": payload[:utm_term]) unless payload[:utm_term].nil?
+        set_sender("POST", "integrations/quotes", data)
       end
     end
   end
--- lib/mobysuite/version.rb
+++ lib/mobysuite/version.rb
@@ -1,3 +1,3 @@
 module Mobysuite
-  VERSION = "0.1.15"
+  VERSION = "0.1.16"
 end
