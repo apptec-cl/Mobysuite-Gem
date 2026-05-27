207f2a3056f22d39107075abf77b6a89d570e238
Matias Menares <dejaaymts@gmail.com>
2026-05-26 22:27:35 -0400

    ADD: Ver 0.3.11

--- lib/mobysuite/gc2/contract.rb
+++ lib/mobysuite/gc2/contract.rb
@@ -19,6 +19,41 @@ module Mobysuite
           query_string = params.empty? ? "" : "?#{params.join('&')}"
           
           return set_sender("GET", "integrations/contracts#{query_string}")
+        end
+
+        def reverse data = {}
+          payload = {}
+
+          if data[:contract_id].nil?
+            payload.merge!(numeroBien: data[:numero_bien]) 
+            payload.merge!(tipoBienEnum: data[:tipo_bien])
+          else
+            payload.merge!(contractId: data[:contract_id])
+          end
+
+          data.merge!(tipo: data[:tipo]) unless payload[:dni].nil?
+          payload.merge!(numeroBien: data[:numero_bien]) unless data[:numero_bien].nil?
+          payload.merge!(tipoBienEnum: data[:tipo_bien]) unless data[:tipo_bien].nil?
+          payload.merge!(tipo: data[:tipo]) unless data[:tipo].nil?
+          payload.merge!(monto: data[:monto]) unless data[:monto].nil?
+          payload.merge!(multa: data[:multa]) unless data[:multa].nil?
+          payload.merge!(multaMonedaLocal: data[:multa_moneda_local]) unless data[:multa_moneda_local].nil?
+          payload.merge!(devolucion: data[:devolucion]) unless data[:devolucion].nil?
+          payload.merge!(retencion: data[:retencion]) unless data[:retencion].nil?
+          payload.merge!(observacion: data[:observacion]) unless data[:observacion].nil?
+
+          return set_sender("POST", "integrations/contracts/reverse")
+        end
+
+      def change_state data = {}
+        if data[:contract_id].nil?
+          payload.merge!(numeroBien: data[:numero_bien]) 
+          payload.merge!(tipoBienEnum: data[:tipo_bien])
+        else
+          payload.merge!(contractId: data[:contract_id])
+        end
+        payload.merge!(nuevoEstado: data[:contract_state])
+        return set_sender("POST", "integrations/contracque me pasats/sync-state", payload)
       end
     end
   end
